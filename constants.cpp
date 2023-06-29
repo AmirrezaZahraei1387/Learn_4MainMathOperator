825b00627d0afa38adefecb520592f4b1ef2cdc5
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-29 13:18:14 -0600

    the constants are created

---
+++ constants.cpp
@@ -0,0 +1,9 @@
+//
+// Created by KAVOSH on 6/29/2023.
+//
+#include "constants.h"
+namespace cons{
+    [[maybe_unused]]extern const int  NUMBER_ASKING{10};
+    [[maybe_unused]]extern const int WINNIG_SCORE{5};
+    [[maybe_unused]]extern const int LOSING_SCORE{-4};
+}// constants
--- constants.h
+++ constants.h
@@ -3,8 +3,10 @@
 //
 #ifndef LEARN_4MAINMATHOPERATOR_CONSTANTS_H
 #define LEARN_4MAINMATHOPERATOR_CONSTANTS_H
-namespace cons{
-    inline constexpr int NUMBER_ASKING {10};
 
+namespace cons{
+    [[maybe_unused]]extern const int  NUMBER_ASKING;
+    [[maybe_unused]]extern const int WINNIG_SCORE;
+    [[maybe_unused]]extern const int LOSING_SCORE;
 }//cons
 #endif //LEARN_4MAINMATHOPERATOR_CONSTANTS_H
