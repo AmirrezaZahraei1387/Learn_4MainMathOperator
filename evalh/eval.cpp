b08503dccf939d5fb4dd7ffc62ba3a4bb9809458
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-06-29 13:25:29 -0600

    the precalc created

---
+++ evalh/eval.cpp
@@ -0,0 +1,26 @@
+//
+// Created by KAVOSH on 6/29/2023.
+//
+
+
+namespace precalc {
+    template<typename NumericalType>
+    NumericalType add(NumericalType number_1, NumericalType number_2) {
+        return number_1 + number_2
+    }
+
+    template<typename NumericalType>
+    NumericalType sub(NumericalType number_1, NumericalType number_2) {
+        return number_1 + number_2
+    }
+
+    template<typename NumericalType>
+    NumericalType dev(NumericalType number_1, NumericalType number_2) {
+        return number_1 + number_2
+    }
+
+    template<typename NumericalType>
+    NumericalType mul(NumericalType number_1, NumericalType number_2) {
+        return number_1 + number_2
+    }
+}// precalc
